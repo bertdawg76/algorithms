1f0a91de11b059363240e59615d4118015bded65
MIHIR PATEL <mihirptl89@gmail.com>
2015-07-30 07:22:32 -0600

    intro to javascript

---
+++ intro_to_javascript/01_javascript.js
@@ -0,0 +1,20 @@
+//I. What is Object-oriented programming?
+/*Object-oriented programming (OOP) is a programming language model organized around objects rather than "actions" and data rather than logic. Historically, a program has been viewed as a logical procedure that takes input data, processes it, and produces output data.*/
+
+//II. What is Javascript?
+/*An object-oriented computer programming language commonly used to create interactive effects within web browsers.*/
+
+//III. Is JavaScript and Java the same?
+/*JavaScript and Java are completely different languages, both in concept and design.
+JavaScript was invented by Brendan Eich in 1995, and became an ECMA standard in 1997.
+ECMA-262 is the official name. 
+ECMAScript 5 (JavaScript 1.8.5 - July 2010) is the current standard.*/
+
+//IV. Is Javascript a scripting language?
+/*JavaScript is a prototype-based scripting language with dynamic typing and first-class functions.*/
+
+//V. What are first class functions?
+/*In computer science, a programming language is said to have first-class functions if it treats functions as first-class citizens. */
+
+// What is first-class citizens?
+/*In programming language design, a first-class citizen (also type, object, entity, or value) in a given programming language is an entity which supports all the operations generally available to other entities.*/
